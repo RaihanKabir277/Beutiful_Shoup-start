427236548261e8fb6b2680447d008a90f0ac1487
RaihanKabir277 <raihank0198@gmail.com>
2025-09-24 16:38:51 +0600

    deifferent quries for fetching the html element and tag

--- main.py
+++ main.py
@@ -13,6 +13,29 @@ soup = BeautifulSoup(contents, 'html.parser')
 # print(soup.prettify())    # give the html code in readble version
 # print(soup.li) #will give the first list if html file
 
+#  ---------- for fetching the all of the value like any tag -------
+all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
 
+# ------ for find the anchor tag text & href-------
+for tag in all_anchor_tags:
+    # print(tag.get_text())
+    # print(tag.get("href"))
+    pass
+
+heading = soup.find(name="h1", id="name")
+# print(heading.string)
+section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.get_text())
+
+#  ------------- for fetch the 1st url -----------
+company_url = soup.select_one(selector="p a")
+# print(company_url.get("href"))
+
+name = soup.select_one(selector="#name")
+# print(name)
+
+heading = soup.select(selector=".heading")
+# print(heading)
 
 
