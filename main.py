f62a154b3076f91ff002c5cbd162e256858bdc5d
RaihanKabir277 <raihank0198@gmail.com>
2025-09-24 16:05:12 +0600

    load the html file and fetch the value in main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 #  ----------------------- day 45 -------------
+from bs4 import BeautifulSoup
+# import lxml   # if html.parser is not working 
+
+with open("website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, 'html.parser')
+# print(soup)
+# print(soup.title)
+# print(soup.title.name)
+# print(soup.title.string)
+# print(soup.prettify())    # give the html code in readble version
+# print(soup.li) #will give the first list if html file
+
+
+
 
